b9b3ffd108c746cd8f1475ec98e80c19377e2a9d
DeyanTopalov <deyan.topalow@gmail.com>
2024-03-04 16:38:23 +0200

    project setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+
+function withOpacity(variableName) {
+  return ({ opacityValue }) => {
+    if (opacityValue !== undefined) {
+      return `hsla(var(${variableName}), ${opacityValue});`;
+    }
+    return `hsla(var($variableName}))`;
+  };
+}
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -7,6 +17,14 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        "clr-gray-blue-200": withOpacity("--gray-blue-200"),
+        "clr-gray-blue-400": withOpacity("--gray-blue-400"),
+        "clr-gray-blue-600": withOpacity("--gray-blue-600"),
+        "clr-gray-blue-800": withOpacity("--gray-blue-800"),
+        "clr-gradient-start": withOpacity("--gradient-start"),
+        "clr-gradient-end": withOpacity("--gradient-end"),
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
